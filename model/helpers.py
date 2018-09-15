36b9c5987356a0bb8f2863d43b6ebd30b6cc1425
Edwin Zhang <edwinzhang64@gmail.com>
2018-09-15 13:08:04 -0400

    Add basic model

---
+++ model/helpers.py
@@ -0,0 +1,24 @@
+from keras import backend as K
+
+
+# euclidean distance for contrastive loss
+def euclidean_dist(vectors):
+  x, y = vectors
+  sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+  return K.sqrt(K.maximum(sum_square, K.epsilon()))
+
+# returns output shape for euclidiean distance
+def euclidean_dist_output_shape(shapes):
+  shape1, shape2 = shapes
+  return (shape1[0], 1)
+
+# contrastive loss function for one-shot learning
+def contrastive_loss(y_actual, y_pred):
+  margin = 1
+  square_pred = K.square(y_pred)
+  margin_square = K.square(K.maximum(margin - y_pred, 0))
+  return K.mean(y_actual * square_pred + (1 - y_actual) * margin_square)
+
+# classification using contrastive loss iwth fixed threshold for distance
+def accuracy(y_actual, y_pred):
+  return K.mean(K.equal(y_actual, K.cast(y_pred < 0.5, y_actual.dtype)))
--- model/model.py
+++ model/model.py
@@ -1,9 +1,43 @@
-from keras.layers import (Activation, Conv2D, Dense, Flatten, Input,
-                          MaxPooling2D)
-from keras.models import Sequential
+import h5py
+from keras import layers, models, optimizers
+from keras.applications import MobileNet
 
-model = Sequential()
+from helpers import accuracy, contrastive_loss, euclidean_dist, euclidean_dist_output_shape
 
-model.add(Flatten(input_shape=(224, 224))), # dimensions of the image
-model.add(Conv2D(filters=64, kernel_size=(7, 7), stride=2))
-model.add(MaxPooling2D(pool_size=(3, 3), stride=2))
+epochs = 20
+input_shape = (224, 224, 3)
+
+def create_base_network(input_shape):
+  # initialize pre-trained base model for fine tuning
+  mobile_net_base = MobileNet(include_top=False,
+                               weights='imagenet',
+                               input_shape = (224, 224, 3)
+                              )
+
+  # freeze everything except last 3 layers for training
+  for layer in mobile_net_base.layers[:-3]:
+    layer.trainable = False
+
+  model = models.Sequential()
+  input = layers.Input(shape=(224, 224, 3))
+
+  # add pre-trained model
+  model.add(mobile_net_base)
+
+  # add new fully-connected layers
+  model.add(layers.Flatten())
+  model.add(layers.Dense(1024, activation='relu'))
+  model.add(layers.Dropout(0.1))
+  model.add(layers.Dense(128, activation='relu'))
+  model.add(layers.Dropout(0.1))
+  model.add(layers.Dense(128, activation='relu'))
+
+  # Show a summary of the model. Check the number of trainable parameters
+  model.summary()
+
+  distance = layers.Lambda(euclidean_dist, output_shape=euclidean_dist_output_shape)()
+
+  rms = optimizers.RMSprop()
+  model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
+
+  model.save('../data/palm_vein.h5')
