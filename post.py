ba843e2d561a546f6aa6e39ab7bb3ed2ec810301
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-09-15 15:57:27 -0400

    Added post functions

--- post.py
+++ post.py
@@ -2,8 +2,18 @@ import requests
 import base64
 url = "http://192.168.43.153:8000/"
 
-with open("pic.jpg", "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
-    payload = {'img': encoded_string}
-    r = requests.post(url, data=payload)
-    print r
+def postOriginal():
+    postImage("pic")
+
+def postProcessed():
+    postImage("thr")
+
+def postImage(name):
+    with open(name + ".jpg", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        payload = {name: encoded_string}
+        requests.post(url, data=payload)
+
+def postLabel(label, isAuthenticated):
+    payload = {"label": label, "auth": isAuthenticated}
+    requests.post(url, data=payload)
