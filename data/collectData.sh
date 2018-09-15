f6d8e3521ee8cd3eede18232148465a45b61ff2f
Ayush Kapur <ayushkapur10@gmail.com>
2018-09-15 17:31:12 -0400

    Updated data collection

--- data/collectData.sh
+++ data/collectData.sh
@@ -5,16 +5,11 @@ while :
 do
     echo 'Enter name (lowercase)'
 	read name
-    #do right data collection
-    for i in {1..10}
+    #do right training data collection
+    for i in {0..5}
     do
     fileNameRight = "%name_right_%i.jpg"
-    raspistill -h 600 -w 600 -roi 0.45,0.57,0.18,0.18 -o %fileNameRight
-    done
-    #do left data collection
-    for i in {1..10}
-    do
-    fileNameLeft = "%name_left_%i.jpg"
-    raspistill -h 600 -w 600 -roi 0.45,0.57,0.18,0.18 -o %fileNameLeft
+    raspistill -h 600 -w 600 -roi 0.45,0.57,0.18,0.18 -o "raw/%fileNameRight"
+    python -c"from ../processing import processing; processing.processImage("raw/%fileNameRight", "processed/%fileNameRight")"
     done
 done
