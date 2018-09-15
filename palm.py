9a4d9f057bf73f1af6cc29d868ba01d65760c9b4
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-09-15 09:48:14 -0400

    noise reduction

--- palm.py
+++ palm.py
@@ -3,3 +3,9 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from PIL import Image
+
+def reduceNoise():
+     # noise
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    noise = cv2.fastNlMeansDenoising(gray)
+    return cv2.cvtColor(noise, cv2.COLOR_GRAY2BGR)
