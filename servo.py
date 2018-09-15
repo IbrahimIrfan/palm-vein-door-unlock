9e3e832b94e9370404961f46f63f4e78b24b1414
Ayush Kapur <ayushkapur10@gmail.com>
2018-09-15 14:15:33 -0400

    Added servo movement functions

---
+++ servo.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+def initGPIO():
+    GPIO.setmode(GPIO.BOARD)
+    #servo signal is on pin18
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setwarnings(False)
+    #set signal frequency to 50hz
+    servo = GPIO.PWM(18, 50)
+    servo.start(10.3)
+    return servo
+
+def actuate(servo):
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(5.6)
+    time.sleep(0.5)
+
+def reset(servo):
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(10.3)
+    time.sleep(0.5)
+
+def cleanup(servo):
+    servo.stop()
+    GPIO.cleanup
+
