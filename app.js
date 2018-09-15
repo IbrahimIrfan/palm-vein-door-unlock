cc492f93d0b7900553e7869750a560c302217817
IbrahimIrfan <1ibrahimirfan@gmail.com>
2018-09-15 15:22:09 -0400

    Completed basic server

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8000;
+const fsPath = require('fs-path');
 
-app.get('/', (req, res) => {
-	images = [req.body.img1, req.body.img2]
-
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 })
 
-app.listen(8000, () => console.log('Server listening on 8000'))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.post('/', (req, res) => {
+	img = req.body.img
+	processed = req.body.processed
+	fsPath.writeFile('images/image.jpeg', img, "base64", function(err){
+    	console.log("File saved to images/");
+    });
+	fsPath.writeFile('images/processed.jpeg', processed, "base64", function(err){
+    	console.log("File saved to images/");
+    });
+	res.send("done");
+});
+
+app.listen(port, () => console.log('listening on port ' + port));
