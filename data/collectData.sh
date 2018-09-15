ab093896cc5465db05ada83100e0c14dc5d4fa3e
Ayush Kapur <ayushkapur10@gmail.com>
2018-09-15 18:31:57 -0400

    Finalized data collection system

--- data/collectData.sh
+++ data/collectData.sh
@@ -6,11 +6,21 @@ do
     echo 'Enter name (lowercase)'
     read name
     #do right training data collection
-    for i in {0..5}
+    for i in {0..4}
     do
     	fileNameRight=${name}_right_${i}.jpg
     	echo $fileNameRight
     	raspistill -h 600 -w 600 -roi 0.45,0.57,0.18,0.18 -o "raw/$fileNameRight"
-    	python -c "from processing import processImage; processImage("raw/${fileNameRight}", "processed/${fileNameRight}")"
+    done
+
+    echo "switch to left hand, press enter"
+    read garbage
+
+    #do left training data collection
+    for i in {0..4}
+    do
+    	fileNameLeft=${name}_left_${i}.jpg
+    	echo $fileNameLeft
+    	raspistill -h 600 -w 600 -roi 0.45,0.57,0.18,0.18 -o "raw/$fileNameLeft"
     done
 done
