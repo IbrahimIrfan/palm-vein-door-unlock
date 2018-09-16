ad540f5ffb930014f854615bd8657a38b408bc54
Edwin Zhang <edwinzhang64@gmail.com>
2018-09-16 01:12:03 -0400

    Update classifier

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import subprocess
 
+import numpy as np
 from keras.models import load_model
+from keras.utils.generic_utils import CustomObjectScope
+from keras.applications import mobilenet
 
 import cv2
 from post import post_label, post_original, post_processed
-from processing import process_image, load_img
+from processing import load_img, process_image
 from servo import actuate, cleanup, init_GPIO, reset
 
 servo = init_GPIO()
@@ -12,6 +15,11 @@ model_path = "./data/palm_vein_model.h5"
 raw_img_path = "pic.jpg"
 processed_img_path = "thr.jpg"
 anchor_img_path = "./data/processed/ayush_right_1.jpg"
+classes = ['angad_left', 'angad_right', 'anushka_left', 'anushka_right', 'ayush_left', 'ayush_right', 
+            'cindy_left', 'cindy_right', 'david_left', 'david_right', 'edwin_left', 'edwin_right', 
+            'ibrahim_left', 'ibrahim_right', 'jason_left', 'jason_right', 'jun_left', 'jun_right', 
+            'justin_left', 'justin_right', 'nick_left', 'nick_right', 'samir_left', 'samir_right', 
+            'thomas_left', 'thomas_right', 'will_left', 'will_right']
 
 while(True):
     input = raw_input("Ready.\n")
@@ -22,14 +30,17 @@ while(True):
         post_processed()
 
         input_img = load_img(processed_img_path, (224, 224))
-        anchor_img = load_img(anchor_img_path, (224, 224))
+        
+        with CustomObjectScope({'relu6': mobilenet.relu6,'DepthwiseConv2D': mobilenet.DepthwiseConv2D}):
+            model = load_model(model_path)
+            y_pred = model.predict(input_img)
+            max_index = np.argmax(y_pred[0])
+            label = classes[max_index]
 
-        model = load_model(model_path)
-        y_pred = model.predict([input_img, anchor_img])
         isAuthenticated = False
-        if y_pred < 0.2:
+        if label == 'ayush_right' and y_pred[0][max_index] > 0.2:
             isAuthenticated = True
-            
+
         post_label(label, isAuthenticated)
         if(isAuthenticated):
             actuate(servo)
--- model/softmax_classifier.py
+++ model/softmax_classifier.py
@@ -1,11 +1,14 @@
 import h5py
 import numpy as np
 from keras import backend as K
-from keras.applications import MobileNet
-from keras.layers import Dense, Dropout, GlobalAveragePooling2D, Input, Reshape, Flatten
-from keras.models import Sequential
+from keras.applications import MobileNet, mobilenet
+from keras.layers import (Dense, Dropout, Flatten, GlobalAveragePooling2D,
+                          Input, Reshape)
+from keras.models import Sequential, load_model
 from keras.optimizers import SGD
 from keras.utils import to_categorical
+from keras.utils.generic_utils import CustomObjectScope
+from keras.preprocessing.image import ImageDataGenerator
 
 from data_generator import DataGenerator
 
@@ -14,8 +17,8 @@ from data_generator import DataGenerator
 def create_base_model(input_shape, num_classes):
   # initialize pre-trained base model for fine tuning
   mobile_net_base = MobileNet(include_top=False,
-                               weights='imagenet',
-                               input_shape = (224, 224, 3)
+                               weights=None,
+                               input_shape=(224, 224, 3)
                              )
   # freeze everything except last 3 layers for training
   for layer in mobile_net_base.layers[:-3]:
@@ -29,9 +32,7 @@ def create_base_model(input_shape, num_classes):
   model.add(GlobalAveragePooling2D())
   model.add(Reshape((1, 1, 1024)))
   model.add(Flatten())
-  model.add(Dense(1024, activation='relu'))
-  model.add(Dropout(0.5))
-  model.add(Dense(1024, activation='relu'))
+  model.add(Dense(512, activation='relu'))
   model.add(Dropout(0.5))
   model.add(Dense(num_classes, activation='softmax'))
   model.summary()
@@ -50,8 +51,9 @@ def get_data(data_path, test_data_path):
   f.close()
   return (x_train, y_train), (x_test, y_test), num_classes
 
-epochs = 20
-batch_size = 32
+
+epochs = 15
+batch_size = 8
 input_shape = (224, 224, 3)
 data_path = '../data/dataset.h5'
 test_data_path = '../data/test_dataset.h5'
@@ -63,20 +65,46 @@ classes = ['angad_left', 'angad_right', 'anushka_left', 'anushka_right', 'ayush_
             'justin_left', 'justin_right', 'nick_left', 'nick_right', 'samir_left', 'samir_right', 
             'thomas_left', 'thomas_right', 'will_left', 'will_right']
 
+
 (x_train, y_train), (x_test, y_test), num_classes = get_data(data_path, test_data_path)
-one_hot_labels = to_categorical(y_train, num_classes=num_classes)
-model = create_base_model(input_shape, num_classes)
+y_train = to_categorical(y_train, num_classes=num_classes)
+y_test = to_categorical(y_test, num_classes=num_classes)
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+train_generator = datagen.flow(x_train, y_train)
+val_generator = datagen.flow(x_test, y_test)
+
+with CustomObjectScope({'relu6': mobilenet.relu6,'DepthwiseConv2D': mobilenet.DepthwiseConv2D}):
+  model = load_model('../data/palm_vein_model.h5')
+
+sgd = SGD(lr=0.01, decay=1e-9, momentum=0.9, nesterov=True)
 model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, one_hot_labels, batch_size=batch_size, epochs=epochs)
+model.fit_generator(train_generator,
+                    steps_per_epoch=x_train.shape[0] // batch_size,
+                    epochs=epochs,
+                    validation_data=val_generator,
+                    validation_steps=x_test.shape[0] // batch_size)
 
-one_hot_test_labels = to_categorical(y_test, num_classes=num_classes)
-score = model.evaluate(x_test, one_hot_test_labels, batch_size=batch_size)
-print("Accuracy + Loss:", score)
+score = model.evaluate(x_test, y_test, batch_size=batch_size)
+print("[Loss, Accuracy]:", score)
 
-predictions = model.predict(x_test)
-print(predictions)
-print(classes[np.argmax(predictions[0])])
+'''
 
+with CustomObjectScope({'relu6': mobilenet.relu6,'DepthwiseConv2D': mobilenet.DepthwiseConv2D}):
+  model = load_model('../data/palm_vein_model.h5')
+
+  predictions = model.predict(x_test)
+  for prediction in predictions:
+    print(np.argmax(prediction))
+    print(classes[np.argmax(prediction)])
+'''
 model.save(model_output_path)
